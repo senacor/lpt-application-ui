67cadb390d6aac310d8a7ea3b6bc3bbfebd7da66
patrick.siegel <patrick.siegel@senacor.com>
2024-11-18 08:04:13 +0100

    - Rm comments

--- src/credit-application/models/credit-decision.type.ts
+++ src/credit-application/models/credit-decision.type.ts
@@ -1,8 +1,6 @@
 import type { CreditDecisionResult } from '@/credit-application/models/credit-decision-result.enum'
 
 export type CreditDecision = {
-  // the uuid of the request, most likely returned from the backend
   id: string
-  // the result of the credit application validation
   decision: CreditDecisionResult | null
 }
