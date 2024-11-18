79d9c88c6ccc8215fd1e81aa9b34ab2fe051c34e
Patrick Siegel <patrick.siegel@senacor.com>
2024-11-18 14:14:34 +0100

    - Adapt to actual service response stucture (#6)
    - Adapt to actual service response structure
    - Use string backed enum for decision status

--- src/credit-application/models/credit-decision.type.ts
+++ src/credit-application/models/credit-decision.type.ts
@@ -1,8 +1,6 @@
 import type { CreditDecisionResult } from '@/credit-application/models/credit-decision-result.enum'
 
 export type CreditDecision = {
-  // the uuid of the request, most likely returned from the backend
-  uuid: string
-  // the result of the credit application validation
+  id: string
   decision: CreditDecisionResult | null
 }
